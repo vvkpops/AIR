ecbc135d5142e8638c504d3e7694f3632e532fbf
Karthik Venkatesan <69886196+vvkpops@users.noreply.github.com>
2025-08-19 21:58:51 -0230

    Add files via upload

---
+++ tailwind-config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          850: '#1a202c',
+          950: '#0d1117'
+        },
+        cyan: {
+          350: '#67e8f9'
+        },
+        orange: {
+          550: '#fb923c'
+        }
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
